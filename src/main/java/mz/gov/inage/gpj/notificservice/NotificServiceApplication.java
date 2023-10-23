a7782bcd6b1cf577963edab6243a462d76f77732
e1000son <emilsonvontade@gmail.com>
2023-10-23 11:11:23 +0200

    Initial project setup

---
+++ src/main/java/mz/gov/inage/gpj/notificservice/NotificServiceApplication.java
@@ -0,0 +1,13 @@
+package mz.gov.inage.gpj.notificservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mz/gov/inage/gpj/notificservice/NotificServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.gov.inage.gpj.notificservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
